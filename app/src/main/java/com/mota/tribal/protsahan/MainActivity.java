9903714110795d10476a634a44ed0a562d57b61f
ayush <ayushjain17aug@gmail.com>
2018-03-22 18:27:30 +0530

    Queries

--- app/src/main/java/com/mota/tribal/protsahan/MainActivity.java
+++ app/src/main/java/com/mota/tribal/protsahan/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.mota.tribal.protsahan.Helper.BottomNavigationViewHelper;
 import com.mota.tribal.protsahan.Schemes.View.SchemeActivity;
@@ -21,8 +20,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -30,19 +27,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_scheme:
                     Intent intent = new Intent(MainActivity.this, SchemeActivity.class);
                     startActivity(intent);
                 case R.id.navigation_profiles:
-                    mTextMessage.setText(R.string.title_profiles);
                     return true;
                 case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_account);
                     return true;
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
                     return true;
             }
             return false;
@@ -58,8 +51,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         initCollapsingToolbar();
-
-        mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         BottomNavigationViewHelper.disableShiftMode(navigation);
